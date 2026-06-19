c84a76e24813ff8e536be31c70b0015fb3b3db4e
Tina Ghoochani <fateme.ghoochani@gmail.com>
2026-06-19 20:15:06 +0200

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,10 @@
 import { globalIgnores } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
 
-const eslintConfig = [
-  ...nextCoreWebVitals,
-  ...nextTypescript,
-  globalIgnores([".next/**", "next-env.d.ts"]),
+export default [
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
 ];
-
-export default eslintConfig;
